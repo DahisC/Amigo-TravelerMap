4a71e0cefaa3b6907810fe0c72408b0077987eb9
ceciliaLioooo <yellowwormmy@gmail.com>
2021-05-25 22:50:13 +0800

    UpdateRoute

--- routes/web.php
+++ routes/web.php
@@ -18,43 +18,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+    //地圖
+    //?search=
+    Route::get('/maps','AmigoController@index')->name('map.index');
 
+    //註冊登入
+    Route::prefix('/sign-in')->group(function () {
+        Route::get('/','AmigoController@index')->name('sign-in.get');
+        Route::post('/','AmigoController@index')->name('sign-in.post');
+    });
 
-Route::prefix('/amigo')->group(function () {
+    Route::prefix('/sign-up')->group(function () {
+        Route::get('/','AmigoController@index')->name('sign-up.get');
+        Route::post('/','AmigoController@index')->name('sign-up.post');
+    });
 
-    //前台
-    Route::prefix('front')->group(function () {
-
-        //地圖
-        //?search=
-        Route::get('/maps','AmigoController@index')->name('map.index');
-
-        //註冊登入
-        Route::prefix('/sign-in')->group(function () {
-            Route::get('/','AmigoController@index')->name('sign-in.get');
-            Route::post('/','AmigoController@index')->name('sign-in.post');
-        });
-
-        Route::prefix('/sign-up')->group(function () {
-            Route::get('/','AmigoController@index')->name('sign-up.get');
-            Route::post('/','AmigoController@index')->name('sign-up.post');
-        });
-
-        //個人頁面
-        Route::prefix('/travelers/{id}')->group(function () {
-            Route::get('/','AmigoController@create')->name('travelers.index');
-            Route::get('/profile','AmigoController@create')->name('travelers.profile');
-            Route::get('/maps','AmigoController@index')->name('travelers.maps');
-            //商人
-            Route::resource('/attractions','AmigoController@index')->except('show');
-        });
-        
-        //我關注的地點
-        Route::get('/itineraries','AmigoController@index')->name('itineraries.index');
+    //個人頁面
+    Route::prefix('/travelers/{id}')->group(function () {
+        Route::get('/','AmigoController@create')->name('travelers.index');
+        Route::get('/profile','AmigoController@create')->name('travelers.profile');
+        Route::get('/maps','AmigoController@index')->name('travelers.maps');
+        //商人
+        Route::resource('/attractions','AmigoController@index')->except('show');
     });
     
+    //我關注的地點
+    Route::get('/itineraries','AmigoController@index')->name('itineraries.index');
+
     //後台
     Route::prefix('/admin')->group(function () {
         Route::get('/','AmigoController@index');
     });
-});
