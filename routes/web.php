72a998da2b819f20fa547c97838bf2c43b9aa46e
ceciliaLioooo <82007945+ceciliaLioooo@users.noreply.github.com>
2021-05-25 23:04:11 +0800

    B cecilia (#1)
    CreateRoutes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+// use view;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+    //地圖
+    //?search=
+    Route::get('/maps','AmigoController@index')->name('map.index');
+
+    //註冊登入
+    Route::prefix('/sign-in')->group(function () {
+        Route::get('/','AmigoController@index')->name('sign-in.get');
+        Route::post('/','AmigoController@index')->name('sign-in.post');
+    });
+
+    Route::prefix('/sign-up')->group(function () {
+        Route::get('/','AmigoController@index')->name('sign-up.get');
+        Route::post('/','AmigoController@index')->name('sign-up.post');
+    });
+
+    //個人頁面
+    Route::prefix('/travelers/{id}')->group(function () {
+        Route::get('/','AmigoController@create')->name('travelers.index');
+        Route::get('/profile','AmigoController@create')->name('travelers.profile');
+        Route::get('/maps','AmigoController@index')->name('travelers.maps');
+        //商人
+        Route::resource('/attractions','AmigoController@index')->except('show');
+    });
+    
+    //我關注的地點
+    Route::get('/itineraries','AmigoController@index')->name('itineraries.index');
+
+    //後台
+    Route::prefix('/admin')->group(function () {
+        Route::get('/','AmigoController@index');
+    });
