73c5d823827ccd56c9eaac083426af4039e44c03
Daniel-Handsome <adwxsghu@gmail.com>
2021-05-30 00:04:04 +0800

    刪掉opentime 討論改成用string

--- database/migrations/2021_05_27_043909_create_attractions_table.php
+++ database/migrations/2021_05_27_043909_create_attractions_table.php
@@ -22,25 +22,20 @@ class CreateAttractionsTable extends Migration
             $table->string('ticket_info');
             $table->longText('traffic_info');
             $table->string('parking_info')->nullable();
+            $table->string('opentime');
 
-            $table->unsignedBigInteger('user_id')->default(0)->index();
+            $table->unsignedBigInteger('user_id')->default(0)->index()->comment('店家的user_id');
 
             $table->foreign('user_id')
-                    ->references('id')
-                    ->on('users')
-                    ->onDelete('cascade');
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
 
             $table->unsignedBigInteger('position_id')->index();
             $table->foreign('position_id')
-                    ->references('id')
-                    ->on('attraction_positions')
-                    ->onDelete('cascade');
-
-            $table->unsignedBigInteger('opentime_id')->index();
-            $table->foreign('opentime_id')
-                    ->references('id')
-                    ->on('attraction_opentimes')
-                    ->onDelete('cascade');
+                ->references('id')
+                ->on('attraction_positions')
+                ->onDelete('cascade');
 
             $table->timestamps();
         });
