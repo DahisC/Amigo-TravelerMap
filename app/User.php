bb6b64df8080e42aedff87916ced44f072f3fbd9
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-02 23:16:31 +0800

    factory一些建立 都測試過

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function map()
+    {
+        return $this->hasMany('App\Map');
+    }
 }
---
+++ database/factories/MapsFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Map;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Map::class, function (Faker $faker) {
+    return [
+        'name'=>$faker->name,
+        'user_id'=>User::find(1),
+    ];
+});
+$factory->afterCreating(Map::class, function ($map, $faker) {
+    return [
+        $map->user()->associate(factory(App\User::class))->make(),
+    ];
+});
