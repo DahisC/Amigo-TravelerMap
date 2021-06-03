6a5e37541ac19ea6142890e42d091cbd503fd5ed
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-03 23:11:32 +0800

    改整齊

--- routes/web.php
+++ routes/web.php
@@ -18,28 +18,26 @@ use App\Http\Controllers\ItinerarieController;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+//主頁
+Route::view('/', 'index')->name('home');
 
 // 地圖
-// ?search=
 Route::resource('/maps', 'MapController');
 
-// 登入與註冊
+// 登入
 Route::view('/sign-in', 'sign-in')->name('sign-in');
+//註冊
 Route::view('/sign-up', 'sign-up')->name('sign-up');
 
 // 個人頁面
 //as 是name
-//prefix url
 Route::group([
     'prefix' => 'travelers',
     'as' => 'traveler.'
 ], function () {
     Route::get('/', 'AmigoController@create')->name('index');
     Route::get('/profile', 'AmigoController@create')->name('profile');
-    // Route::get('/maps', 'AmigoController@index')->name('traveler.maps');
+    Route::get('/maps', 'AmigoController@index')->name('traveler.maps');
     //商人
     Route::resource('/attractions', 'AttractionController')->except('show');
     //middleware
