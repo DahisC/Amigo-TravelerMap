f27f43c4e4058e6b68c9d30f82a1176931118f6a
DahisC <dahischeng@gmail.com>
2021-06-04 17:22:58 +0800

    rewrote seeders so that we can seeding our db by 1 line code

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'Admin',
+            'password' => bcrypt('a')
+        ]);
+    }
+}
