4f15f551675d1390d3ffa94ed04daddfffc66548
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-05 10:14:24 +0800

    軟刪除

--- app/Attraction.php
+++ app/Attraction.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attraction extends Model
 {
+    use SoftDeletes;
     protected $table = 'attractions';
 
     protected $fillable = [
--- app/AttractionImage.php
+++ app/AttractionImage.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AttractionImage extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'url', 'image_desc', 'attraction_id',
     ];
