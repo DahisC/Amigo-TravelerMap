d3bd558eb57f1651981e795e45fc45425adab88a
snowfishchocolate <82297315+snowfishchocolate@users.noreply.github.com>
2021-06-08 21:14:53 +0800

    0608
    1.切首頁
    2.在public的image資料夾放照片
    3.有跑過bootstrap5 npm

--- resources/views/Snow/test.blade.php
+++ resources/views/Snow/test.blade.php
@@ -1,5 +1,13 @@
-@extends('layouts.amigo');
-@section('css')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <style>
     * {
       box-sizing: border-box;
@@ -7,89 +15,101 @@
       padding: 0;
     }
 
-    body {
-      background-color: #f9f6ed;
+    .carousel-item {
+      width: 100%;
+      height: 650px;
+      display: flex;
+      position: relative;
+      background-attachment: fixed;
     }
 
-    .jumbotron {
+    .carousel-item img {
       width: 100%;
-      height: 700px;
-      background-color: gray;
+      background-position: center;
+      background-size: cover;
     }
 
     .logo {
-      width: 151px;
-      height: 151px;
+      width: 100px;
+      height: 100px;
       border-radius: 50%;
-      margin: 0 auto;
-      background-color: blue
+      background-color: blue;
+      position: absolute;
+      left: 50%;
+      top: 10%;
+      transform: translateX(-50%);
     }
 
     .button {
-      width: 514px;
+      width: 300px;
+      margin: 0 auto;
+      position: absolute;
+      left: 50%;
+      top: 35%;
+      transform: translateX(-50%);
       display: flex;
       justify-content: space-between;
-      font-size: 30px;
-      color: black;
-      margin: 0 auto;
     }
 
-    .trapezoid {
-      background-image: url(./public/snow-image/trapezoid.png);
-      width: 100%;
-      height: fit-content;
-      background-color: #6b8123;
+    .icon {
+      width: 500px;
+      position: absolute;
+      left: 75%;
+      top: 5%;
     }
 
-    .text {
-      color: white;
-      font-size: 36px;
+    .icon img {
+      width: 30px;
+      margin-right: 20px;
     }
 
-    .col h3 {
-      color: white;
-      font-size: 36px;
-      margin: 0 auto;
+    .icon img:hover {
+      border: 2px solid rgb(0, 183, 255);
     }
 
-    .tra-pic {
-      width: 469px;
-      height: 549px;
+    .story .col img {
+      width: 100px;
+      height: 200px;
     }
 
   </style>
-@endsection
+</head>
 
-@section('content')
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <div class="logo"></div>
-      <div class="button">
-        <a href="" class="to">前往地圖</a>
-        <a href="" class="collect">收藏地點</a>
-        <a href="" class="edit">編輯個人地圖</a>
-      </div>
-    </div>
-  </div>
-  <div class="trapezoid">
-    <div class="container ">
-      <div class="row">
-        <div class="col">
-          <img src="" alt="" class="tra-pic">
+<body>
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="{{ asset('./image/sea.jpg') }}">
+        <div class="logo"></div>
+        <div class="button">
+          <a href="">前往地圖</a>
+          <a href="">前往地圖</a>
+          <a href="">前往地圖</a>
         </div>
-        <div class="col">
-          <h3>關於地圖</h3>
-          <p></p>
-          <div class="text">
-            不論是在獸皮上刻畫線條，還是在螢幕上使用繪圖軟體，幾千年來人類不斷用地圖來描述、回溯或是理解這個世界。而今，貝西‧梅森和葛瑞格‧米勒這兩位獲獎記者，以及國家地理網站熱門部落格All Over The
-            Map格主，憑著對地圖的無比熱情，帶你穿越時空，回到過去也前往未來，環遊世界並飛上穹蒼──而且完全是透過地圖迷人的藝術與科學。
-          </div>
+        <div class="icon">
+          <img src="{{ asset('./image/human.png') }}" alt="" class="human" style="width: 40px">
+          <img src="{{ asset('./image/facebook-logo.png') }}" alt="">
+          <img src="{{ asset('./image/line-logo.png') }}" alt="">
         </div>
       </div>
     </div>
   </div>
-@endsection
+  <div class="container story">
+    <div class="row">
+      <div class="col">
+        <img src="{{ asset('./image/luggage.jpg') }}" alt="">
+      </div>
+      <div class="col">
+        地圖文案
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+    integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous">
+  </script>
+</body>
 
-@section('js')
-
-@endsection
+</html>
