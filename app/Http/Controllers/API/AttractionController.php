269dd3dd811bbcac632b9eda6b92c8d04922cd52
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-10 00:57:30 +0800

    修改api

--- app/Http/Controllers/API/AttractionController.php
+++ app/Http/Controllers/API/AttractionController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ApiAttractionController extends Controller
+class AttractionController extends Controller
 {
     /**
      * Display a listing of the resource.
