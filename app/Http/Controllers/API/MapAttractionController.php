89ad08516841894daabb8f6bab061f2f28584ed1
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-13 22:39:59 +0800

    卡住了

--- app/Http/Controllers/API/MapAttractionController.php
+++ app/Http/Controllers/API/MapAttractionController.php
@@ -27,6 +27,7 @@ class MapAttractionController extends Controller
      */
     public function store(Request $request)
     {
+        //
     }
 
     /**
@@ -49,11 +50,13 @@ class MapAttractionController extends Controller
      */
     public function update(Request $request, $id)
     {
+
+        //delete create
         $map = Map::findOrFail($id);
         $attraction = Attraction::findOrFail($request->id);
-        $map->attractions()->attach($attraction);
+        $map->attractions()->syncWithoutDetaching($attraction);
 
-        return ['map' => $map];
+        return ['map'=>$map];
     }
 
     /**
