9ce4633c2b421718d6ee26906d0fe2f9c06d142f
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-13 19:21:15 +0800

    補上

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //如果取attraction他會先連外面的controller 我才加api
 Route::name('api.')->group(function(){
     // 請求附近的景點
-   Route::apiResource('/attractions','API\AttractionController');
+   Route::apiResource('/attractions','API\AttractionController')->only(['index']);
    // 收藏的地點
    Route::apiResource('/favorites','API\UserAttractionController');
    // 地圖上加入感興趣的地點
-   Route::apiResource('/mapAttractions','API\MapAttractionController');
+   Route::apiResource('/mapAttractions','API\MapAttractionController')->only(['update','destroy']);
 });
 
 
