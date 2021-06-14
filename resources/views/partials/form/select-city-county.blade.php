7cd8243a15722ea20a062279d6c1c6edc7808d76
DahisC <dahischeng@gmail.com>
2021-06-14 19:43:38 +0800

    fixed a bug that can't open search modal

--- resources/views/partials/form/select-city-county.blade.php
+++ resources/views/partials/form/select-city-county.blade.php
@@ -1,5 +1,5 @@
 <div id="city-county-selector" class="col-12 mb-3">
-  <label for="search-form__input_range" class="form-label">位置</label>
+  <label for="search-form__input_range" class="form-label">位置123</label>
   <div class="row">
     <div class="col-12 mb-3 d-none">
       <label for="search-form__input_range" class="form-label">
@@ -9,15 +9,15 @@
     </div>
     <div class="col-6">
       <label for="search-form__input_range" class="form-label">
-        <small>縣市</small>
+        <small>縣市2</small>
       </label>
-      <select id="select_city" name="region" class="form-select"></select>
+      <select id="select_city" class="form-select"></select>
     </div>
     <div class="col-6">
       <label for="search-form__input_range" class="form-label">
-        <small>鄉鎮區</small>
+        <small>鄉鎮區3</small>
       </label>
-      <select id="select_area" name="town" class="form-select"></select>
+      <select id="select_area" class="form-select"></select>
     </div>
     <input id="zipcode" type="text" hidden />
   </div>
