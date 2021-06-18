c7dede5f6dafb7878ef58ce5682363eddfb0e767
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-18 14:26:01 +0800

    update跟delete合併

--- app/Http/Controllers/API/MapAttractionController.php
+++ app/Http/Controllers/API/MapAttractionController.php
@@ -9,21 +9,20 @@ use App\Http\Controllers\Controller;
 
 class MapAttractionController extends Controller
 {
-    public function update(Request $request,Map $map)
+    public function update(Request $request, $id)
     {
         //delete create
-        
-        $attraction = Attraction::findOrFail($request->id);
-        $map->attractions()->syncWithoutDetaching($attraction);
+        $map = Map::findOrFail($id);
+        $attraction = Attraction::findOrFail($request->attraction_id);
+        //重點 用數量去判斷
+        //我猜測關聯表的主key 不是id 所以會不知道找去哪
+        $has = Map::where('id',$id)->whereHas('attractions', function ($query) use ($attraction) {
+            $query->where('attraction_id', $attraction->id);
+        })->get()->count();
+        // dd($has);
+        if (!$has) $map->attractions()->attach($attraction);
+        else $map->attractions()->detach($attraction);
 
-        return ['map'=>$map];
-    }
-
-    public function destroy(Request $request,Map $map)
-    {
-        $attraction = Attraction::findOrFail($request->id);
-        $map->attractions()->detach($attraction);
-
-        return ['map'=>$map];
+        return ['map' => $map];
     }
 }
