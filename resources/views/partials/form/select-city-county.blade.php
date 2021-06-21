0b4ebceb47c6e09cb8c291b0665c0a4b3c9be69f
DahisC <dahischeng@gmail.com>
2021-06-21 23:35:58 +0800

    unimportant

--- resources/views/partials/form/select-city-county.blade.php
+++ resources/views/partials/form/select-city-county.blade.php
@@ -25,4 +25,14 @@
 
 @prepend('stack-js')
 <script src="{{ asset('js/twCitySelector.js') }}"></script>
+<script>
+  new TwCitySelector({
+    el: '#city-county-selector',
+    elCounty: '#select_city', // 在 el 裡查找 element
+    elDistrict: '#select_area', // 在 el 裡查找 element
+    elZipcode: '#zipcode', // 在 el 裡查找 element
+    countyFieldName: 'region',
+    districtFieldName: 'town'
+  });
+</script>
 @endprepend
