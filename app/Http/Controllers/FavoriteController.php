9d8c82e18338430bee9f2bef0af567e75e89c108
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-22 23:36:33 +0800

    Update FavoriteController.php

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -15,33 +15,31 @@ class FavoriteController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
-            $inputText ='蕃茄';
-            $userFavorites = User::with([
-                'attractions',
-                'attractions.images',
-                'attractions.position',
-                'attractions.time',
-                'attractions.tags'
-            ])->findOrFail(auth()->user()->id)->attractions;
+        $userFavorites = User::with([
+            'attractions',
+            'attractions.images',
+            'attractions.position',
+            'attractions.time',
+            'attractions.tags'
+        ])->findOrFail(auth()->user()->id)->attractions;
 
-            $userFavorites = $userFavorites->filter(function ($favorite) use ($inputText) {
-               
-                return false !== stristr($favorite->name, $inputText);
-            });
-
-            if (auth()->check()) {
-                // dd(auth()->user()->id);
-                $userFavorites = User::with([
-                    'attractions',
-                    'attractions.images',
-                    'attractions.position',
-                    'attractions.time',
-                    'attractions.tags'
-                ])->findOrFail(auth()->user()->id)->attractions->paginate(10);
-                return view('favorites.index', compact('userFavorites'));
-            }
+        switch ($request->search) {
+            case 'text':
+                $inputText = '蕃茄';
+                $userFavorites = $userFavorites->filter(function ($favorite) use ($inputText) {
+                    return false !== stristr($favorite->name, $inputText);
+                });
+            case 'tag':
+                $tag ='景點';
+                $userFavorites = $userFavorites->whereHas('tags',function($tag){
+                    $tag->where('tag_id',);
+                });
+        };
+        dd($userFavorites);
+        $userFavorites->paginate(10);
+        return view('favorites.index', compact('userFavorites'));
         // $tag = Tag::where('name',)
         // Session::flash('toast-test', collect(['type' => 'danger', 'header' => '權限不足', 'body' => '測試']));
         // if (Gate::allows('viewAny', Attraction::class)) { // Bug: 在有登入的情況下，這邊的 if 不會執行
@@ -58,6 +56,6 @@ class FavoriteController extends Controller
         //         return redirect()->route('sign-in');
         //     }
         // }
-       
+
     }
 }
