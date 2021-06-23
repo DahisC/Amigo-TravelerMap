579a991ce41d21534e491dafd7535bf36a238d52
Daniel-Handsome <adwxsghu@gmail.com>
2021-06-23 19:45:07 +0800

    大頭照我改回來  用樣板字串他會把$當變數

--- app/Http/Controllers/Backstage/AttractionController.php
+++ app/Http/Controllers/Backstage/AttractionController.php
@@ -16,39 +16,43 @@ class AttractionController extends Controller
     public function index()
     {
         $user = Auth::user();
-        if (Gate::allows('viewAny',Attraction::class)) {
-            if($user->role == "Admin"){
+        if (Gate::allows('viewAny', Attraction::class)) {
+            if ($user->role == "Admin") {
                 $attractions = Attraction::paginate(10);
-                return view('backstage.attractions.index',compact('attractions'));
-            }else{
-                $attractions = Attraction::where('user_id',$user->id)->paginate(10);
-                return view('backstage.attractions.index',compact('attractions'));
+                return view('backstage.attractions.index', compact('attractions'));
+            } else {
+                $attractions = Attraction::where('user_id', $user->id)->paginate(10);
+                return view('backstage.attractions.index', compact('attractions'));
             }
         }
         return view('backstage.index'); //很抱歉，您的權限不足，發送火箭享尊榮服務
     }
 
-   
+
 
     public function create()
     {
-        if (Gate::allows('view-guider',Attraction::class)) {
+        if (Gate::allows('view-guider', Attraction::class)) {
             $tags = Tag::get();
             return view('backstage.attractions.factory', compact('tags'));
         }
         return view('backstage.index'); //很抱歉，您的權限不足，發送火箭享尊榮服務
     }
 
-    public function edit(Attraction $attraction)
+    public function edit($id)
     {
-        $user = Auth::user();
         $tags = Tag::get();
-        if(Gate::allows('viewAny',$attraction)){
-            if($user->role == "Admin"){
+        $attraction = Attraction::with([
+            'position',
+            'images',
+            'tags'
+        ])->find($id);
+        if (Gate::allows('viewAny', $attraction)) {
+            if (auth()->user()->role == "Admin") {
                 // dd($attractions);
                 return view('backstage.attractions.factory', compact('attraction', 'tags'));
             }
-            if(Gate::allows('update',$attraction)){
+            if (Gate::allows('update', $attraction)) {
                 return view('backstage.attractions.factory', compact('attraction', 'tags'));
             }
         }
