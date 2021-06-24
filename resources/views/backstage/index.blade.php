478474633c41890a9fba5799bbb39cda0a561916
DahisC <dahischeng@gmail.com>
2021-06-24 22:46:38 +0800

    unimportant

--- resources/views/backstage/index.blade.php
+++ resources/views/backstage/index.blade.php
@@ -64,17 +64,14 @@
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
+    @if (auth()->user()->role !== 'Traveler')
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-info shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                @if (auth()->user()->role === 'Traveler')
                 嚮導專屬
-                @else
-                上傳地點
-                @endif
               </div>
               <div class="row no-gutters align-items-center">
                 <div class="col-auto">
@@ -93,6 +90,7 @@
         </div>
       </div>
     </div>
+    @endif
 
     <!-- Pending Requests Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
