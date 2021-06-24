70e131cfd765a949748f0ffb39d1dd2ef429892c
DahisC <dahischeng@gmail.com>
2021-06-24 23:23:07 +0800

    added href on logo

--- resources/views/partials/backstage/sidebar.blade.php
+++ resources/views/partials/backstage/sidebar.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
   <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('homepage') }}">
     {{-- <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-map-marked"></i>
     </div> --}}
